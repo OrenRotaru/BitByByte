dd1f70c8487dfcb45de2dd25f0d7ff0385ce3cb6
Oren Rotaru <64712576+OrenRotaru@users.noreply.github.com>
2023-12-05 18:01:00 -0600

    modified serverjs

--- Backend/server.js
+++ Backend/server.js
@@ -9,17 +9,58 @@ const UserRoutes = require("./routes/UserRoutes");
 const MessageRoutes = require("./routes/MessageRoutes");
 const SearchRoutes = require("./routes/SearchRoutes");
 const nano = require("nano")(process.env.COUCHDB_URL);
-const messagesDb = nano.use('messages');
 
 // middleware
 app.use(cors());
-app.use(express.json({ limit: "50mb" }))
+app.use(express.json({ limit: "50mb" }));
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
+// create a messages, channels, users database if we don't have one already
+nano.db
+  .create("messages")
+  .then(() => {
+    console.log("Created messages database");
+  })
+  .catch((err) => {
+    console.error("Error creating messages database", err);
+  });
 
+nano.db
+  .create("channels")
+  .then(() => {
+    console.log("Created channels database");
+  })
+  .catch((err) => {
+    console.error("Error creating channels database", err);
+  });
+
+nano.db
+  .create("users")
+  .then(() => {
+    console.log("Created users database");
+  })
+  .catch((err) => {
+    console.error("Error creating users database", err);
+  });
+
+  
+const messagesDb = nano.use("messages");
+// Create index on likesCount
+messagesDb
+  .createIndex({
+    index: {
+      fields: ["likesCount"],
+    },
+  })
+  .then(() => {
+    console.log("Index created successfully");
+  })
+  .catch((err) => {
+    console.error("Error creating index", err);
+  });
 
 
 // routes
@@ -32,16 +73,4 @@ app.use("/api/search", SearchRoutes);
 const server = app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
-
-// Create index on likesCount
-messagesDb.createIndex({
-    index: {
-        fields: ['likesCount']
-    }
-}).then(() => {
-    console.log('Index created successfully');
-}).catch((err) => {
-    console.error('Error creating index', err);
-});
-
 
